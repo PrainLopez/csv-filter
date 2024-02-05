8933f6c32a8fd6f44ffb4332a5086a6ac261646c
Prain Lopez <qym200042@gmail.com>
2024-02-05 17:23:07 +0800

    logic undergo

--- main.js
+++ main.js
@@ -1,12 +1,40 @@
+import { decodeStream } from 'iconv-lite'
+import { parse } from 'csv-parse'
+import { createReadStream } from 'fs'
 
-$('button#execute').click(function() {
-    const fileList = Array.from($('input#csv-file').prop('files'));
-    console.log(fileList);
+$('button#execute').click(function () {
+  const fileList = Array.from($('input#csv-file').prop('files'))
+  console.log(fileList)
 
-    if (fileList.length === 0) {
-        alert('Please select a file');
-        return;
-    }
+  if (fileList.length === 0) {
+    alert('请选择文件！')
+    return
+  }
 
-    fileList.forEach((file) => {})
-});
+  $('button#execute').prop('disabled', true)
+
+  const result = Map()
+
+  fileList.forEach((file) => {
+    const readStream = createReadStream(file.path)
+    const gbkStream = decodeStream('gbk')
+    const parseStream = parse({
+      delimiter: ',',
+      record_delimiter: '\n',
+      from_line: 4
+    })
+
+    readStream
+      .pipe(gbkStream)
+      .pipe(parseStream)
+      .on('readable', () => {
+        const filtered = []
+        do {
+            const record = parseStream.read()
+            if (record) {
+              filtered.push(record)
+            }
+        }
+      })
+  })
+})
