1f1d60b0827dd2a75d9cd0c28cb0bc735083cd8e
Prain Lopez <qym200042@gmail.com>
2024-02-06 11:29:11 +0800

    logic unsolved

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+let lineCount = 0
+
+const condition = (data) =>
+  data
+    .slice(6, 9)
+    .every((item) => Number.parseInt(item) <= 0)
 
 $('button#execute').click(function () {
-  const fileList = Array.from($('input#csv-file').prop('files'))
-  console.log(fileList)
+  const fileList = Array.from(
+    $('input#csv-file').prop('files')
+  )
 
   if (fileList.length === 0) {
     alert('请选择文件！')
@@ -10,27 +17,53 @@ $('button#execute').click(function () {
 
   $('button#execute').prop('disabled', true)
 
-  const result = Map()
+  const stepFn = (results, parser) => {
+    if (condition(results.data)) {
+      $('<p></p>')
+        .text(
+          `line ${lineCount}: ${results.data.slice(6, 9)}`
+        )
+        .appendTo('.editing')
+    }
 
-  fileList.forEach((file) => {
+    lineCount += 1
+  }
+
+  const errorFn = (error) => {
+    $('<p></p>')
+      .text(`line ${lineCount}: ${error}`)
+      .addClass('error')
+      .appendTo('.editing')
+
+    lineCount += 1
+  }
+
+  const completeFn = () => {
+    $('button#execute').prop('disabled', false)
+    $('.editing').removeClass('editing')
+    lineCount = 0
+  }
+
+  for (let i = 0; i < fileList.length; i++) {
+    const file = fileList[i]
+
+    var detail = $('<details></details>').addClass(
+      'editing'
+    )
+    detail.append($('<summary></summary>').text(file.name))
+    $('.list').append(detail)
+
+    lineCount = 4
     Papa.parse(file, {
-      delimiter: ",",
+      delimiter: ',',
       dynamicTyping: true,
-      encoding: "GBK",
-      step: undefined,
-      complete: undefined,
-      error: undefined,
-      download: false,
-      downloadRequestHeaders: undefined,
-      downloadRequestBody: undefined,
-      skipEmptyLines: false,
-      chunk: undefined,
-      chunkSize: undefined,
-      fastMode: undefined,
+      encoding: 'GBK',
+      step: stepFn,
+      complete: completeFn,
+      error: errorFn,
       beforeFirstChunk: undefined,
-      withCredentials: undefined,
       transform: undefined,
-      skipFirstNLines: 0
+      skipFirstNLines: lineCount - 1
     })
-  })
+  }
 })
